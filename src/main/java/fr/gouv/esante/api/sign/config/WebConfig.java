06c5e5c3f5216dad2f8909b7e43c0a8fba49a527
mjuravliov <Maxim.JURAVLIOV.EXT@esante.gouv.fr>
2023-11-29 09:30:54 +0100

    test springdoc

--- src/main/java/fr/gouv/esante/api/sign/config/WebConfig.java
+++ src/main/java/fr/gouv/esante/api/sign/config/WebConfig.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -27,7 +29,9 @@ public class WebConfig implements WebMvcConfigurer {
      */
     @Override
     public void configureMessageConverters(final List<HttpMessageConverter<?>> converters) {
-        converters.add(new MappingJackson2HttpMessageConverter());
+    	//converters.add(new StringHttpMessageConverter());
+    	converters.add(new ByteArrayHttpMessageConverter());
+    	converters.add(new MappingJackson2HttpMessageConverter());
     }
 
     /*
